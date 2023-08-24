379ae78ca8ada4c79016a449272ad52b71621748
Philani Ncube <ncbphi001@gmail.com>
2023-08-24 19:55:51 +0200

    hide hero elements

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -45,17 +45,17 @@ const Hero = () => {
           width={1920}
           height={1080}
           alt="Watermark"
-          className="w-[70%] lg:-translate-y-[400px] opacity-60  lg:translate-x-[550px] object-cover"
+          className="w-[65%] lg:-translate-y-[200px] opacity-70  lg:translate-x-[600px] object-cover"
         />
       </div>
       <section className="container relative h-[80vh] py-6 rounded-md shadow-md bg-white/80 overflow-clip">
         <div className="flex flex-col h-full gap-4 md:flex-row">
           <div className="flex flex-col items-start justify-center w-full h-full text-black lg:-translate-y-8">
-            <h1 className="text-3xl font-bold md:text-5xl lg:text-6xl">
+            <h1 className="text-3xl hidden font-bold md:text-5xl lg:text-6xl">
               Your partner in tax compliance
             </h1>
 
-            <Link href="/contact">
+            <Link href="/contact" className="hidden">
               <Button className="mt-3 text-xs tracking-wider uppercase bg-black rounded-none md:text-md font-raleway w-fit">
                 Get a free consultation today!
               </Button>
@@ -67,11 +67,11 @@ const Hero = () => {
               height="1024"
               src="/images/Africa.webp"
               alt="people"
-              className="object-cover scale-[90%] lg:w-full -translate-y-16 lg:-translate-y-6 translate-x-10 grayscale aspect-square"
+              className="object-cover w-full  grayscale aspect-square"
             />
           </div>
         </div>
-        <div className="hidden lg:grid grid-cols-1 gap-4 my-4 -translate-y-[200px] sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 max-w-[80%]">
+        {/* <div className="hidden lg:grid grid-cols-1 gap-4 my-4 -translate-y-[200px] sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 max-w-[80%]">
           {services.map((service, index) => (
             <div key={index} className="w-full px-2 py-5 rounded shadow-md bg-slate-200/90">
               <h3 className="mb-2 text-sm font-extrabold ">{service.name}</h3>
@@ -80,7 +80,7 @@ const Hero = () => {
               <Link href={service.href} className="font-bold text-xs bg-black hover:bg-slate-900 text-white mt-5 rounded-full px-4 py-2">Read More</Link>
             </div>
           ))}
-        </div>
+        </div> */}
       </section>
     </div>
   );
